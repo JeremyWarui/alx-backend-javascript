cfb093499cc72132098477427179f983fbd9422d
JeremyWarui <jerywarne7@gmail.com>
2023-06-28 23:16:41 +0300

    Try/catch finally

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,11 +3,11 @@ export default function guardrail(mathFunction) {
   let answer;
   try {
     answer = mathFunction();
+    queue.push(answer);
   } catch (error) {
     answer = error.toString();
-  } finally {
-    queue.push(answer);
-    queue.push('Guardrail was processed');
   }
+  queue.push(answer);
+  queue.push('Guardrail was processed');
   return queue;
 }
