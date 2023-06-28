8c93bc4adf512318f8fd2c759b6cb5e14d4c1bcb
JeremyWarui <jerywarne7@gmail.com>
2023-06-28 22:41:22 +0300

    Fix import errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise === true) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-  }).catch(() => console.log('Got a response from the API'));
+  return promise.then(() => (
+    {
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
