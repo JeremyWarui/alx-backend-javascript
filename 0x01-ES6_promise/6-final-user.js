9031edf5977f76cb584fc82fa31109968fbf6cb2
JeremyWarui <jerywarne7@gmail.com>
2023-06-28 23:40:14 +0300

    RUn all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,34 @@
-/* eslint-disable import/named */
-/* eslint-disable import/extensions */
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((vals) => [
-      {
-        status: vals[0],
-        value: vals[0],
-      },
-    ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = {
+    status: 'pending',
+    value: '',
+  };
+
+  try {
+    const newUser = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = newUser;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
+  }
+
+  const photo = {
+    status: 'fulfilled',
+    value: '',
+  };
+
+  try {
+    const newPhoto = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = newPhoto;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+
+  return [user, photo];
 }
