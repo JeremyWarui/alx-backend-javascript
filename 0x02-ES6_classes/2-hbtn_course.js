f55e00ca3ec7ca8c75ee7e1d3f723a5fbb35d54c
JeremyWarui <jerywarne7@gmail.com>
2023-07-01 17:52:29 +0300

    Sky high building

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,34 +1,36 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof (newName) !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
-  }
-
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    if (typeof (newLength) !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = newLength;
+  get students() {
+    return this._students;
   }
 
-  get students() {
-    return this.students;
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 
   set students(newStudents) {
