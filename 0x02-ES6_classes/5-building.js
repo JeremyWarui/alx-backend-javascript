cd8cdbc188d14d891c82f7467372afff3c0c94a1
JeremyWarui <jerywarne7@gmail.com>
2023-07-01 18:28:02 +0300

    Sky high building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== 'Building' && typeof this.evacuationWarningMessage !== 'function') {
+    if (this.constructor.name !== 'Building'
+    && typeof this.evacuationWarningMessage !== 'function'
+    ) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
