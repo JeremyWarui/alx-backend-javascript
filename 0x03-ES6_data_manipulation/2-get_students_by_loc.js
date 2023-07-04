7e23d138dd09167ec774af0996791af811bcbf22
JeremyWarui <jerywarne7@gmail.com>
2023-07-04 23:37:31 +0300

    Tasks on Sets, Array methods and TYped arrays

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(array, city) {
-  if (Array.isArray(array)) {
-    return array.filter((item) => item.location === city);
-  }
+  return array.filter((item) => item.location === city);
 }
