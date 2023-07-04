6a12c5636ab2869a870f0d99b4c31d2c0062d508
JeremyWarui <jerywarne7@gmail.com>
2023-07-05 00:42:14 +0300

    Tasks on Sets, Array methods and TYped arrays

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString.length > 0) {
+  if (startString.length > 0 && set instanceof Set) {
     const array = [];
     for (const item of set) {
-      if (item.startsWith(startString)) {
+      if (item && item.startsWith(startString)) {
         array.push(item.slice(startString.length));
       }
     }
