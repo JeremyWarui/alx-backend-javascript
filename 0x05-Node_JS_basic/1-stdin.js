058e984af7f6869d25c9abf298d6c6cd424a9c74
JeremyWarui <jerywarne7@gmail.com>
2023-08-25 02:53:51 -0700

    STDIN

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) {
+  if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
