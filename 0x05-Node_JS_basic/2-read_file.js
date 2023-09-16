3141448515dbc06c6abab1b4fabea2e984832c01
JeremyWarui <jerywarne7@gmail.com>
2023-09-16 14:38:50 +0300

    Fixing errors on readfile code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,28 +3,23 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     const content = fs.readFileSync(path).toString().split('\n');
-    let students = content.filter((item) => item);
-    students = students.map((item) => item.split(','));
+    const students = content.filter((item) => item).map((item) => item.split(','));
 
     const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
     console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 
-    const rows = {};
-    for (const row in students) {
-      if (row !== 0) {
-        if (!rows[students[row][3]]) rows[students[row][3]] = [];
-        rows[students[row][3]].push(students[row][0]);
+    const fields = {};
+    for (let i = 1; i < students.length; i += 1) {
+      const field = students[i][3];
+      if (fields[field]) {
+        fields[field].push(students[i][0]);
+      } else {
+        fields[field] = [students[i][0]];
       }
     }
 
-    delete rows.row;
-
-    for (const key of Object.keys(rows)) {
-      console.log(
-        `Number of students in ${key}: ${rows[key].length}. List: ${rows[
-          key
-        ].join(', ')}`,
-      );
+    for (const [key, val] of Object.entries(fields)) {
+      console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
