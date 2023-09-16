a0cf39885ae033bcc924d810239cd8c17656561f
JeremyWarui <jerywarne7@gmail.com>
2023-09-16 15:20:23 +0300

    Read file using nodejs server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,25 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const arg = process.argv[2];
-
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.write('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
 
     try {
-      const data = await countStudents(arg);
-      res.end(`${data.join('\n')}`);
+      countStudents(process.argv[2])
+        .then((data) => res.end(data));
     } catch (error) {
       res.end(error.message);
     }
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
-  res.end();
 });
 
 app.listen(1245);
