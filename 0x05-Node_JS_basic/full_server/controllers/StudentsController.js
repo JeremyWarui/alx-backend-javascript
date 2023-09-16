16098125c3b3ea30e146dcedd490b3713f49368d
JeremyWarui <jerywarne7@gmail.com>
2023-09-16 19:26:59 +0300

    Full server in Nodejs

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,7 +1,7 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents (request, response, DATABASE) {
+  static getAllStudents(request, response, DATABASE) {
     readDatabase(DATABASE)
       .then((fields) => {
         const output = [];
@@ -20,7 +20,7 @@ class StudentsController {
       });
   }
 
-  static getAllStudentsByMajor (request, response, DATABASE) {
+  static getAllStudentsByMajor(request, response, DATABASE) {
     const { major } = request.params;
     if (major !== 'CS' && major !== 'SWE') {
       response.send(500, 'Major parameter must be CS or SWE');
