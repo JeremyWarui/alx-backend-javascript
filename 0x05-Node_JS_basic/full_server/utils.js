b661a4ece541806be3aaef86f38bcd6524f32c25
JeremyWarui <jerywarne7@gmail.com>
2023-09-16 19:14:05 +0300

    Full server in Nodejs

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 
-const readDatabase = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) reject(Error(error));
-    }
-    const content = data.toString().split('\n');
-    let students = content.filter((item) => item);
-    students = students.map((item) => item.split(','));
-    const fields = {};
-    for (const i in students) {
-      if (i !== 0) {
-        if (!fields[students[i][3]]) fields[students[i][3]] = [];
-        fields[students[i][3]].push(students[i][0]);
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (error, data) => {
+    if (error) {
+      reject(error);
+    } else {
+      const content = data.toString().split('\n');
+      const students = content.filter((item) => item).map((student) => student.split(','));
+      const fields = {};
+
+      for (let i = 1; i < students.length; i += 1) {
+        const field = students[i][3];
+
+        if (fields[field]) {
+          fields[field].push(students[i][0]);
+        } else {
+          fields[field] = [students[i][0]];
+        }
       }
+      resolve(fields);
     }
-    delete fields.field;
-    resolve(fields);
-  }));
-};
-export default readDatabase;
+  });
+});
+
+module.exports = readDatabase;
