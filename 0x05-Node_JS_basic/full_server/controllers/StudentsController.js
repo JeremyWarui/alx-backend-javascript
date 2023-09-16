ccd834b8a5ef47091ebe7fba41eb4303f4d66cd6
JeremyWarui <jerywarne7@gmail.com>
2023-09-16 19:24:57 +0300

    Full server in Nodejs

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,9 +1,9 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents(request, response, DATABASE) {
-    try {
-      readDatabase(DATABASE).then((fields) => {
+  static getAllStudents (request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
         const output = [];
         const title = 'This is the list of our students';
         output.push(title);
@@ -14,25 +14,25 @@ class StudentsController {
           output.push(message);
         }
         response.send(200, `${output.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
       });
-    } catch (error) {
-      response.send(500, 'Cannot load the database');
-    }
   }
 
-  static getAllStudentsByMajor(request, response, DATABASE) {
-    try {
-      const { major } = request.params;
-      if (major !== 'CS' && major !== 'SWE') {
-        response.send(500, 'Major parameter must be CS or SWE');
-      } else {
-        readDatabase(DATABASE).then((fields) => {
+  static getAllStudentsByMajor (request, response, DATABASE) {
+    const { major } = request.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(DATABASE)
+        .then((fields) => {
           const students = fields[major];
           response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => {
+          response.send(500, 'Cannot load the database');
         });
-      }
-    } catch (error) {
-      response.send(500, 'Cannot load the database');
     }
   }
 }
