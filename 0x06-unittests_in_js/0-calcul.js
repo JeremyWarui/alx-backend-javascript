97aec433d40aa7692e445ac507d05d4b203bb095
JeremyWarui <jerywarne7@gmail.com>
2023-09-20 07:05:48 +0000

    Test suite for a calculateNumber function

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -6,8 +6,4 @@ function calculateNumber(a, b) {
   return roundedA + roundedB;
 }
 
-console.log(calculateNumber(1, 3));
-console.log(calculateNumber(1, 3.7));
-console.log(calculateNumber(1.5, 3.7));
-
 module.exports = calculateNumber;
