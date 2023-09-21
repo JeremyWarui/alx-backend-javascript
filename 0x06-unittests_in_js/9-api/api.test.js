d3e8f330305a481ce4a5f7e19903b44421ae9b3c
JeremyWarui <jerywarne7@gmail.com>
2023-09-21 12:52:25 +0000

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,43 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('index page', () => {
+  it('should return a welcome message', (done) => {
+    const call = {
+      url: 'http://localhost:7865',
+      method: 'GET',
+    };
+    request(call, (error, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('/cart/12', () => {
+  it('should response with 200 and id 12 in message', (done) => {
+    const call = {
+      url: 'http://localhost:7865/cart/12',
+      method: 'GET',
+    };
+    request(call, (error, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+});
+
+describe('/cart/ab2', () => {
+  it('should response with 404', (done) => {
+    const call = {
+      url: 'http://localhost:7865/cart/ab2',
+      method: 'GET',
+    };
+    request(call, (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
