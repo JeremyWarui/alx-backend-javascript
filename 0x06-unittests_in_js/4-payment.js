9475270ec22db1b192ab725c00e3dc42de508de9
JeremyWarui <jerywarne7@gmail.com>
2023-09-26 09:10:24 +0000

    Deleting month 2 tasks

--- 0x06-unittests_in_js/4-payment.js
+++
@@ -1,9 +0,0 @@
-const Utils = require('./utils.js');
-
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const answer = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${answer}`);
-  return answer;
-}
-
-module.exports = sendPaymentRequestToApi;
