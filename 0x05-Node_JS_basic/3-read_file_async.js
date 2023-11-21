9863400195c932de872c9723d11b02d19a5c65a4
JeremyWarui <jerywarne7@gmail.com>
2023-11-21 10:22:26 +0000

    Read file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,3 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) reject(Error('Cannot load the database'));
+      else {
+        const studentData = data.split('\n');
+        const students = studentData.filter((student) => student !== '')
+          .map((student) => student.split(','));
+        students.shift();
+        const noOfStudents = students.length ? students.length : 0;
+        console.log(`Number of students: ${noOfStudents}`);
 
+        const fields = {};
+
+        for (const student of students) {
+          const field = student[3];
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(student[0]);
+        }
+
+        for (const [field, students] of Object.entries(fields)) {
+          console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+        }
+
+        resolve(fields);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
