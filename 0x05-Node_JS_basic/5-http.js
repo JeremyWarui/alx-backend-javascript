21705065eeb36b895232fb7911b8247eb295a0e6
JeremyWarui <jerywarne7@gmail.com>
2023-11-21 12:04:36 +0000

    COMPLEX http server using nodejs http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,28 +1,23 @@
-const http = require('http')
-const countStudents = require('./3-read_file_async')
+const http = require('http');
+
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  const { url } = req
-  switch (url) {
-    case '/':
-      res.write('Hello Holberton School!')
-      break
-    case '/students':
-      res.write('This is the list of our students\n')
-      try {
-        const students = await countStudents(process.argv[2])
-        res.end(`${students.join('\n')}`)
-      } catch (err) {
-        res.end(err.message)
-      }
-      break
-    default:
-      res.write('Hello Holberton School!')
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const { url } = req;
+  if (url === '/') res.write('Hello Holberton School!');
+  if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.end(`${students.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
+    }
   }
-  res.end()
-})
+  res.end();
+});
 
-app.listen(1245)
+app.listen(1245);
 
-module.exports = app
+module.exports = app;
