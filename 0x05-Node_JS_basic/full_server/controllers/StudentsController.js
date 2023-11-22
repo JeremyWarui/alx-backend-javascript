37cec0fbe18a62eab7443350c03b2fe9181d6a88
Jeremy <jerywarne7@gmail.com>
2023-11-22 22:49:26 +0300

    Fix students route error

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,20 +2,29 @@ import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(request, response, DB) {
-    readDatabase(DB)
-      .then((fields) => {
-        const students = [];
-        let msg;
+    readDatabase(DB).then((fields) => {
+      const students = [];
+      let msg = 'This is the list of our students';
+      students.push(msg);
 
-        students.push('This is the list of our students');
+      for (const [field, list] of Object.entries(fields)) {
+        msg = `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
+        students.push(msg);
+      }
+      response.status(200).send(`${students.join('\n')}`);
+    }).catch(() => response.status(500).send('Cannot load the database'));
+    // readDatabase(DB).then((fields) => {
+    //   const students = [];
+    //   let msg;
 
-        for (const [field, list] of Object.entries(fields)) {
-          msg = `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
-          students.push(msg);
-        }
-        response.status(200).status(`${students.join('\n')}`);
-      })
-      .catch(() => response.status(500).send('Cannot load the database'));
+    //   students.push('This is the list of our students');
+
+    //   for (const [field, list] of Object.entries(fields)) {
+    //     msg = `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
+    //     students.push(msg);
+    //   }
+    //   response.status(200).status(`${students.join('\n')}`);
+    // }).catch(() => response.status(500).send('Cannot load the database'));
   }
 
   static getAllStudentsByMajor(request, response, DB) {
@@ -29,7 +38,6 @@ class StudentsController {
         response.status(200).send(`List: ${students.join(', ')}`);
       }).catch(() => response.status(500).send('Cannot load the database'));
     }
-    //response.status(200);
   }
 }
 
