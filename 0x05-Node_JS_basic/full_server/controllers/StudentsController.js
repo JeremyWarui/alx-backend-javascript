680b23ea3fd0fa4e09ade32111584217a90b61f7
Jeremy <jerywarne7@gmail.com>
2023-11-22 22:50:35 +0300

    Fix students route error

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -13,18 +13,6 @@ class StudentsController {
       }
       response.status(200).send(`${students.join('\n')}`);
     }).catch(() => response.status(500).send('Cannot load the database'));
-    // readDatabase(DB).then((fields) => {
-    //   const students = [];
-    //   let msg;
-
-    //   students.push('This is the list of our students');
-
-    //   for (const [field, list] of Object.entries(fields)) {
-    //     msg = `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
-    //     students.push(msg);
-    //   }
-    //   response.status(200).status(`${students.join('\n')}`);
-    // }).catch(() => response.status(500).send('Cannot load the database'));
   }
 
   static getAllStudentsByMajor(request, response, DB) {
