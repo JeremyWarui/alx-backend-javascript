1852af580c3e75f3b1db8571a085787a0884154e
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-11-23 08:37:04 +0000

    Basic integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,13 @@
-const app = require('./api.js');
 const { expect } = require('chai');
 const request = require('request');
 const endpoint = `http://localhost:7865`;
 
 describe('Test API', () => {
-    it('should return 200 as status code', (done) => {
-        request(endpoint, (err, res, body) => {
-            expect(res.statusCode).to.equal(200);
-            expect(body).to.equal('Welcome to the payment system')
-        });
-        done();
+  it('should return 200 as status code', (done) => {
+    request(endpoint, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system')
     });
+    done();
+  });
 });
